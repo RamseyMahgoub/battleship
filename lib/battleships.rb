25a585c5969878a2e78fcde20dbffba2252399af
Simon Levy <simon.levy@onthebeach.co.uk>
2018-10-17 15:16:40 +0100

    game process

--- lib/battleships.rb
+++ lib/battleships.rb
@@ -1,10 +1,12 @@
 class Battleship
 
-  def initialize
-    @their_ships = [["b1"]]
+  def initialize(grid_size)
+    @their_ships = [[Cell.new("b1")]]
     @their_grid = []
     @my_grid = []
-    @my_ships = [["a1"]]
+    @my_ships = [[Cell.new("a1")]]
+    @my_turn = true
+    @grid_size = grid_size
   end
 
   def my_ships
@@ -20,35 +22,98 @@ class Battleship
   end
 
   def fire(coord)
-    hit = @their_ships.any? do |ship|
-      ship.include?(coord)
-    end
-    @their_grid << Cell.new(coord,hit)
-    hit
-  end
+    turn = Turn.new(coord, @their_grid, @my_grid, @my_turn, @grid_size, @their_ships, @my_ships)
+    sunken_ship = false
+    hit = nil
+    result = false
 
-  def their_fire(coord)
-    hit = @my_ships.any? do |ship|
-      ship.include?(coord)
+    if !turn.error
+      if @my_turn
+        ships = @their_ships
+        grid = @their_grid
+      else
+        ships = @my_ships
+        grid = @my_grid
+      end
+
+      hit = ships.any? do |ship|
+        ship_hit = ship.any? do |cell|
+          cell_hit = cell.coord == coord
+
+          if cell_hit
+            cell.hit = true
+          end
+
+          cell_hit
+        end
+
+        if ship_hit
+          ship_shunk = ship.all? { |cell| cell.hit == true }
+        end
+
+        if ship_shunk
+          sunken_ship = ship
+        end
+
+        ship_hit
+      end
+      grid << Cell.new(coord,hit)
+
+
+      result = ships.all? do |ship|
+        ship.all? do |cell|
+          cell.hit
+        end
+      end
     end
-    @my_grid << Cell.new(coord,hit)
-    hit
+
+    turn.hit = hit
+    turn.sunk = sunken_ship
+    turn.result = result
+
+    turn
   end
 
   def next_turn
+    @my_turn = !@my_turn
+  end
+end
 
+class Turn
+  attr_accessor :hit, :sunk, :result
+
+  def initialize(coord, their_grid, my_grid, my_turn, grid_size, their_ships, my_ships)
+    if their_grid.size > my_grid.size && my_turn
+      @error = 'Not your turn'
+    else
+      xy = coord_to_x_y(coord)
+
+      if (xy[:x] > grid_size || xy[:y] > grid_size)
+        @error = 'Out of grid'
+      end
+    end
   end
 
+  def coord_to_x_y(coord)
+    start = 'a'.ord - 1
+
+    {
+      x: coord.chars[1].to_i,
+      y: coord.chars[0].ord - start,
+    }
+  end
+
+  def error
+    @error
+  end
 end
 
 class Cell
-  def initialize(coord, hit)
+  attr_accessor :hit
+
+  def initialize(coord, hit = false)
     @coord = coord
     @hit = hit
-  end
-
-  def hit
-    @hit
   end
 
   def coord
--- spec/battleships_spec.rb
+++ spec/battleships_spec.rb
@@ -1,46 +1,72 @@
 require 'battleships'
 RSpec.describe 'Battleships' do
+  let (:battleship) { Battleship.new(3) }
+
   it 'should return an array of one ship.' do
-    battleship = Battleship.new
-    expect(battleship.my_ships).to eq([["a1"]])
+    expect(battleship.my_ships[0][0].coord).to eq("a1")
   end
 
   it 'should return an array of hits on my_grid.' do
-    battleship = Battleship.new
     expect(battleship.my_grid).to eq([])
   end
 
   it 'should return an array of hits on their_grid.' do
-    battleship = Battleship.new
     expect(battleship.their_grid).to eq([])
-  end
-  
-  it 'should fire at a grid cell and return miss.' do
-    battleship = Battleship.new
-    expect(battleship.fire("c1")).to eq(false)
   end
 
   it 'should fire at a grid cell and return miss.' do
-    battleship = Battleship.new
-    expect(battleship.fire("c2")).to eq(false)
+    expect(battleship.fire("c1").hit).to eq(false)
+  end
+
+  it 'should fire at a grid cell and return miss.' do
+    expect(battleship.fire("c2").hit).to eq(false)
   end
 
   it 'should fire at a grid cell and return hit.' do
-    battleship = Battleship.new
-    expect(battleship.fire("b1")).to eq(true)
+    expect(battleship.fire("b1").hit).to eq(true)
   end
 
   it 'should return their grid their_grid with shot history.' do
-    battleship = Battleship.new
     battleship.fire("b1")
     expect(battleship.their_grid[0].hit).to eq(true)
     expect(battleship.their_grid[0].coord).to eq("b1")
   end
 
   it 'battleships next_turn should make the computer fire and miss.' do
-    battleship = Battleship.new
     battleship.next_turn
+    battleship.fire("a2")
     expect(battleship.my_grid[0].hit).to eq(false)
     expect(battleship.my_grid[0].coord).to eq("a2")
+  end
+
+  it 'should only allow 1 fire per a turn.' do
+    battleship.fire("a1")
+    battleship.fire("b1")
+    expect(battleship.their_grid.size).to be(1)
+  end
+
+  it 'should return "not your turn" if its not your turn.' do
+    battleship.fire("a1")
+    expect(battleship.fire("b1").error).to eq('Not your turn')
+  end
+
+  it 'should only allow shots within the grid vertically' do
+    expect(battleship.fire("d3").error).to eq('Out of grid')
+  end
+
+  it 'should only allow shots within the grid horizontally' do
+    expect(battleship.fire("c4").error).to eq('Out of grid')
+  end
+
+  it 'should return coords of sunked ship once sunk' do
+    expect(battleship.fire("b1").sunk[0].coord).to eq('b1')
+  end
+
+  it 'should return the result of false when game not finished' do
+    expect(battleship.fire("c1").result).to eq(false)
+  end
+
+  it 'should return the result of true when game has finished' do
+    expect(battleship.fire("b1").result).to eq(true)
   end
 end
