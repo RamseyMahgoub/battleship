83a7df81d523540fd292cf0deffd163f43e152c3
Si Levy <simonjlevy@gmail.com>
2018-10-22 11:01:07 +0100

    added target and change_turn validation, to make sure 1 target happens per a turn and a turn can not be changed unless a target has happened. also made sure target can not happen before setup complete or after game complete

--- app/models/game.rb
+++ app/models/game.rb
@@ -21,22 +21,34 @@ class Game < ApplicationRecord
     game_players.find(id)
   end
 
+  # Refactor: game should not reach into ships
   def result
-    game_players.find do |game_player|
-      game_player.ships.all?(&:sunk?)
+    loser = game_players.find do |game_player|
+      game_player.ships.all?(&:sunk?) && game_player.ships.size > 0
+    end
+
+    if loser
+      return game_players.find { |game_player| game_player.id != loser.id }
     end
   end
 
   def target(coord)
-    get_receive_turn_player.receive_target(coord)
+    return false if game_players.any? { |game_player| game_player.ships.size == 0 }
+    return false if result
+    return get_receive_turn_player.receive_target(coord)
   end
 
+  # Refactor: game should not be setting on game_player
   def change_turn
     active = get_active_turn_player
     receiver = get_receive_turn_player
 
+    return false if !receiver.turn_has_targeted
+
     active.update(active_turn: false)
-    receiver.update(active_turn: true)
+    receiver.update(active_turn: true, turn_has_targeted: false)
+
+    true
   end
 
   private
--- spec/models/game_spec.rb
+++ spec/models/game_spec.rb
@@ -72,30 +72,119 @@ RSpec.describe Game, type: :model do
     game.game_players[1].ships[0].cells.first.update(targeted: true)
     expect(game.result).to be(nil)
     game.game_players[1].ships[0].cells.last.update(targeted: true)
-    expect(game.result).to eq(game_player_2)
+    expect(game.result).to eq(game_player_1)
   end
 
-  it 'target will shoot at a cell' do
+  it 'result will return nil if game is not setup yet' do
     game = Game.create
-    game_player = game.get_player(game.computer_game_player_id)
+    expect(game.result).to be(nil)
+  end
 
-    game.target('F6')
+  it 'target will shoot at a cell and return true if successful' do
+    game = Game.create
+    game_player_1 = game.get_player(game.computer_game_player_id)
+    game_player_2 = game.get_player(game.human_game_player_id)
+    ship_type = ShipType.create(name: 'a', size: 2)
+    Ship.create_on_grid(game_player_1, ship_type, ['B1', 'B2'])
+    ship = Ship.create_on_grid(game_player_2, ship_type, ['C1', 'D1'])
+
+    expect(game.target('F6')).to be(true)
     expect(game.game_players[0].grid.find_cell_by_coord('F6').targeted).to be(true)
   end
 
-  it 'change_turn will change the active turn' do
+  it 'change_turn will change the active turn and return true if successful' do
     game = Game.create
     game_player = game.get_player(game.computer_game_player_id)
 
     expect(game.game_players[0].active_turn).to be(false)
     expect(game.game_players[1].active_turn).to be(true)
-    game.change_turn
+
+    game.game_players[0].update(turn_has_targeted: true)
+    expect(game.change_turn).to be(true)
+
     expect(game.game_players[0].active_turn).to be(true)
     expect(game.game_players[1].active_turn).to be(false)
+  end
+
+  it 'target will return false if game already complete' do
+    game = Game.create
+    game_player_1 = game.get_player(game.computer_game_player_id)
+    game_player_2 = game.get_player(game.human_game_player_id)
+    ship_type = ShipType.create(name: 'a', size: 2)
+    Ship.create_on_grid(game_player_1, ship_type, ['B1', 'B2'])
+    ship = Ship.create_on_grid(game_player_2, ship_type, ['C1', 'D1'])
+
+    game.game_players[1].ships.first.cells.update(targeted: true)
+    expect(game.result).to eq(game_player_1)
+    expect(game.target('G6')).to be(false)
+    expect(game.game_players[0].grid.find_cell_by_coord('G6').targeted).to be(false)
+  end
+
+  it 'target will return false if player already targeted in their turn' do
+    game = Game.create
+    game_player_1 = game.get_player(game.computer_game_player_id)
+    game_player_2 = game.get_player(game.human_game_player_id)
+    ship_type = ShipType.create(name: 'a', size: 2)
+    Ship.create_on_grid(game_player_1, ship_type, ['B1', 'B2'])
+    ship = Ship.create_on_grid(game_player_2, ship_type, ['C1', 'D1'])
+
+    expect(game.target('D8')).to be(true)
+    expect(game.target('C8')).to be(false)
+    expect(game.target('B8')).to be(false)
+    expect(game.game_players[0].grid.find_cell_by_coord('D8').targeted).to be(true)
+    expect(game.game_players[0].grid.find_cell_by_coord('C8').targeted).to be(false)
+    expect(game.game_players[0].grid.find_cell_by_coord('B8').targeted).to be(false)
+  end
+
+  it 'target will return false if player already targeted cell' do
+    game = Game.create
+    game_player_1 = game.get_player(game.computer_game_player_id)
+    game_player_2 = game.get_player(game.human_game_player_id)
+    ship_type = ShipType.create(name: 'a', size: 2)
+    Ship.create_on_grid(game_player_1, ship_type, ['B1', 'B2'])
+    ship = Ship.create_on_grid(game_player_2, ship_type, ['C1', 'D1'])
+
+    game.game_players[0].grid.find_cell_by_coord('D8').update(targeted: true)
+    expect(game.target('D8')).to be(false)
+    expect(game.game_players[0].grid.find_cell_by_coord('D8').targeted).to be(true)
+  end
+
+  it 'target will return false if setup not complete' do
+    game = Game.create
+    game_player_1 = game.get_player(game.computer_game_player_id)
+    game_player_2 = game.get_player(game.human_game_player_id)
+
+    expect(game.target('A8')).to be(false)
+    expect(game.game_players[0].grid.find_cell_by_coord('A8').targeted).to be(false)
+  end
+
+  it 'change_turn will reset the turn targeted flag' do
+    game = Game.create
+    game_player_1 = game.get_player(game.computer_game_player_id)
+    game_player_2 = game.get_player(game.human_game_player_id)
+    ship_type = ShipType.create(name: 'a', size: 2)
+    Ship.create_on_grid(game_player_1, ship_type, ['B1', 'B2'])
+    ship = Ship.create_on_grid(game_player_2, ship_type, ['C1', 'D1'])
+
+    expect(game.target('A8')).to be(true)
+    expect(game.change_turn).to be(true)
+    expect(game.target('F2')).to be(true)
+    expect(game.change_turn).to be(true)
+    expect(game.target('C1')).to be(true)
+  end
+
+  it 'change_turn return false and not rotate turn if no turn target yet' do
+    game = Game.create
+    game_player_1 = game.get_player(game.computer_game_player_id)
+    game_player_2 = game.get_player(game.human_game_player_id)
+
+    expect(game_player_2.active_turn).to be(true)
+    expect(game_player_1.active_turn).to be(false)
+    expect(game.change_turn).to be(false)
+    expect(game_player_2.active_turn).to be(true)
+    expect(game_player_1.active_turn).to be(false)
   end
 end
 
 # TODO
-# stop fire if game complete or already fired
-# stop rotate turn if not fired
-# stop fire before setup complete
+# AI, random ship positing and AI targeting
